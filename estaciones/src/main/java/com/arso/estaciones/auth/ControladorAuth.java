7fd4845c7daaedc08a2e9843aef04d4bb19b11c0
FreddyH124 <hernafmr@gmail.com>
2024-04-09 18:18:53 +0200

    soluciono liada

--- estaciones/src/main/java/com/arso/estaciones/auth/ControladorAuth.java
+++ estaciones/src/main/java/com/arso/estaciones/auth/ControladorAuth.java
@@ -29,34 +29,10 @@ public class ControladorAuth {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginDTO dto) {
         return ResponseEntity.ok(servicioAuth.login(dto));
-        /*Map<String, Object> claims = verificarCredenciales(user, password);
-        if (claims != null) {
-            Date caducidad = Date.from(Instant.now().plusSeconds(3600));
-            String token = Jwts.builder()
-                    .setClaims(claims)
-                    .signWith(SignatureAlgorithm.HS256, "secreto")
-                    .setExpiration(caducidad).compact();
-
-            return ResponseEntity.ok(token);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
-        }*/
     }
 
     @PostMapping("/registro")
     public ResponseEntity<AuthResponse> registro(@RequestBody RegistroDTO dto){
-        System.out.println(dto.getNombre());
-        System.out.println(dto.getClave());
-        System.out.println(dto.getRol());
         return ResponseEntity.ok(servicioAuth.register(dto));
-    }
-
-    private Map<String, Object> verificarCredenciales(String username, String password) {
-        // Aquí iría la lógica de verificación de credenciales
-        // Por simplicidad, siempre se devuelve un usuario administrador
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("sub", username);
-        claims.put("roles", "administrador");
-        return claims;
     }
 }
