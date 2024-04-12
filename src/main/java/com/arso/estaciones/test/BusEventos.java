f0647823c398e9534d1f26c694b1f578a47ea067
FreddyH124 <hernafmr@gmail.com>
2024-04-12 23:54:43 +0200

    solucion

--- src/main/java/com/arso/estaciones/test/BusEventos.java
+++ src/main/java/com/arso/estaciones/test/BusEventos.java
@@ -1,7 +1,9 @@
 package com.arso.estaciones.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BusEventos {
 
 	public static void main(String[] args) {
