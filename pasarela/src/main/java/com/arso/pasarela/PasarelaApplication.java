ecac9d0638777ca5e552fbf8df962ba001221547
FreddyH124 <freddy.hernan@upicus.com>
2024-05-04 12:18:57 +0200

    intentado poner zuul

--- pasarela/src/main/java/com/arso/pasarela/PasarelaApplication.java
+++ pasarela/src/main/java/com/arso/pasarela/PasarelaApplication.java
@@ -2,8 +2,10 @@ package com.arso.pasarela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+//@EnableZuulProxy
 public class PasarelaApplication {
 
 	public static void main(String[] args) {
---
+++ pasarela/src/main/java/com/arso/pasarela/rest/ControladorPasarela.java
@@ -0,0 +1,11 @@
+package com.arso.pasarela.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pasarela")
+public class ControladorPasarela {
+
+
+}
